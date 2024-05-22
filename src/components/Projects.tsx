157c69d21651c2f72a936a44266806efbb0d6a8c
kemiobabitoyosi <kemiobabi.toyosi@gmail.com>
2024-05-22 14:58:40 +0100

    image change

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 import Project1 from "../app/tic-tac-toe.webp";
-import Project2 from "../app/token-swap.webp";
+import Project2 from "../app/token-swap2.webp";
 import Project3 from "../app/toshies-drumkit.webp";
 import Project4 from "../app/whack-a-bear.webp";
 
@@ -15,23 +15,23 @@ const projectsData: Project[] = [
   {
     name: "Spooky Momoguro Tic-Tac-Toe Game",
     imageUrl: `${Project1.src}`,
-    githubLink: "https://spooky-momoguro-tic-tac-toe.vercel.app"
+    githubLink: "https://spooky-momoguro-tic-tac-toe.vercel.app",
   },
   {
     name: "Trade Secure Token Swap",
     imageUrl: `${Project2.src}`,
-    githubLink: "https://github.com/demiladebdm/Blog-Application-Client"
+    githubLink: "https://github.com/Kemiobabitoyosi/Monad-dex.git",
   },
   {
     name: "Toshies DrumKit",
     imageUrl: `${Project3.src}`,
-    githubLink: "https://kemiobabitoyosi.github.io/Toshies-Drumkit"
+    githubLink: "https://kemiobabitoyosi.github.io/Toshies-Drumkit",
   },
   {
     name: "Whack a Bear Game",
     imageUrl: `${Project4.src}`,
-    githubLink: "https://kemiobabitoyosi.github.io/whack-a-bear-bruh"
-  }
+    githubLink: "https://kemiobabitoyosi.github.io/whack-a-bear-bruh",
+  },
 ];
 
 const Projects: React.FC = () => {
@@ -42,7 +42,11 @@ const Projects: React.FC = () => {
         <div className="carousel owl-carousel">
           {projectsData.map((project, index) => (
             <div className="card" key={index}>
-              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+              <a
+                href={project.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <div className="box">
                   <img src={project.imageUrl} alt={project.name} />
                   <div className="text">{project.name}</div>
@@ -52,7 +56,11 @@ const Projects: React.FC = () => {
           ))}
         </div>
         <div className="githubDiv">
-          <a href="https://github.com/demiladebdm" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/demiladebdm"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <span className="github2">More Projects</span>
           </a>
         </div>
@@ -62,4 +70,3 @@ const Projects: React.FC = () => {
 };
 
 export default Projects;
-
