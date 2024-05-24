3cc9e9537d1155324e2ef50f59ebcf7dcd2a0c81
kemiobabitoyosi <kemiobabi.toyosi@gmail.com>
2024-05-24 23:52:34 +0100

    portfolio link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 // components/Navbar.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 interface NavbarProps {
   scrollBelowHomePage: boolean;
@@ -34,18 +34,48 @@ const Navbar: React.FC<NavbarProps> = ({ scrollBelowHomePage }) => {
   };
 
   return (
-    <nav className={`navbar ${menuOpen || isScrolled ? "active" : ""} ${isScrolled && scrollBelowHomePage ? "full-bg" : ""}`}>
+    <nav
+      className={`navbar ${menuOpen || isScrolled ? "active" : ""} ${
+        isScrolled && scrollBelowHomePage ? "full-bg" : ""
+      }`}
+    >
       <div className="max-width">
         <div className="logo">
-          <a href="#">Portfo<span>lio.</span></a>
+          <a href="#home">
+            Portfo<span>lio.</span>
+          </a>
         </div>
         <ul className={`menu ${menuOpen ? "active" : ""}`}>
-          <li><a href="#home" className="menu-btn" onClick={closeMenu}>Home</a></li>
-          <li><a href="#about" className="menu-btn" onClick={closeMenu}>About</a></li>
-          <li><a href="#services" className="menu-btn" onClick={closeMenu}>Services</a></li>
-          <li><a href="#skills" className="menu-btn" onClick={closeMenu}>Skills</a></li>
-          <li><a href="#teams" className="menu-btn" onClick={closeMenu}>Projects</a></li>
-          <li><a href="#contact" className="menu-btn" onClick={closeMenu}>Contact</a></li>
+          <li>
+            <a href="#home" className="menu-btn" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="menu-btn" onClick={closeMenu}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="menu-btn" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="menu-btn" onClick={closeMenu}>
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#teams" className="menu-btn" onClick={closeMenu}>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="menu-btn" onClick={closeMenu}>
+              Contact
+            </a>
+          </li>
         </ul>
         <div className="menu-btn" onClick={toggleMenu}>
           <i className={`fas fa-bars ${menuOpen ? "active" : ""}`}></i>
